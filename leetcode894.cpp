0f624ce6372e51a14920d90879008100a776bbc5
Fan Hong <temperancehong0@gmail.com>
2023-06-27 16:36:02 +0200

    pb 894, with editorial

--- leetcode894.cpp
+++ leetcode894.cpp
@@ -21,9 +21,67 @@ struct TreeNode {
       TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
   };
 
-class Solution {
-public:
-    vector<TreeNode*> allPossibleFBT(int n) {
 
+unordered_map<int, vector<TreeNode*>> memo;
+vector<TreeNode*> allPossibleFBT(int n){
+    if (n%2 == 0){
+        return {};
     }
+    else if (n == 1){
+        return memo[1] = {new TreeNode()};
+    }
+    else{
+        vector<TreeNode*> res;
+        for (int i = 1; i < n; i=i+2){
+            vector<TreeNode*> left = allPossibleFBT(i);
+            vector<TreeNode*> right = allPossibleFBT(n-i-1);
+            for (auto l : left){
+                for (auto j: right){
+                    res.push_back(new TreeNode(0, l, j));
+                }
+            }
+        }
+        return memo[n] = res;
+    }
+}
+
+
+
+// using top down strategy
+//vector<TreeNode*> allPossibleFBT(int n) {
+//    if (n%2 == 0){
+//        vector<TreeNode*> res;
+//        return res;
+//    }
+//    else if (n == 1){
+//        TreeNode* firstTree = new TreeNode();
+//        vector<TreeNode*> res;
+//        res.push_back(firstTree);
+//        return res;
+//    }
+//    else if (n == 3){
+//        vector<TreeNode*> res;
+//        TreeNode* basic1 = new TreeNode();
+//        TreeNode* basic2 = new TreeNode();
+//        TreeNode* firstTree = new TreeNode(0, basic1, basic2);
+//        res.push_back(firstTree);
+//        return res;
+//    }
+//    else{
+//        vector<TreeNode*> base_vec = allPossibleFBT(n-2);
+//        vector<TreeNode*> res_vec;
+//        for (auto tNode:base_vec){
+//            // iterate through every node that is from previous, add it to left or right child to the basic tree
+//            TreeNode* basic1 = new TreeNode();
+//            TreeNode* basic2 = new TreeNode();
+//            TreeNode* add1 = new TreeNode(0, basic1, tNode);
+//            TreeNode* add2 = new TreeNode(0, tNode, basic2);
+//            res_vec.push_back(add1);
+//            res_vec.push_back(add2);
+//        }
+//
+//
+//        return res_vec;
+//    }
+//}
 
