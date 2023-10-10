1e1bd13fb3b692e494173b45183b70bfdff341dc
fan.hong <fan.hong@telecom-paris.fr>
2023-10-10 11:09:50 +0200

    10 Oct easy

--- leetcode88.cpp
+++ leetcode88.cpp
@@ -5,12 +5,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// double pointer, backward, so no element would be missed out
 void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
     int i = n-1;
     int j = n-1;
     int k = n+m-1;
-    while (j >=0 && i >= 0){
-        if (nums1[i] >= nums2[j]){
+    while (j >=0){
+        if (i>= 0 && nums1[i] >= nums2[j]){ // because i can be subtracted to minus
             nums1[k--] = nums1[i--];
         }
         else{
